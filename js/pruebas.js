d543ab1fa7652859ec6d117a83a017c2047513b2
RamunnoAJ <ramunnoagustin@gmail.com>
2022-06-02 16:49:50 -0300

    Crea más validaciones y pruebas
    En este commit cree funciones para validar la ciudad y la descripción del regalo, así como también pruebas para estas validaciones

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,15 +1,40 @@
 function probarValidarNombre() {
   console.assert(
-      validarNombre('') === 'Este campo debe tener al menos 1 caracter',
-      'Validar nombre no validó que el nombre no sea vacío',
+    validarNombre("") === "Este campo debe tener al menos 1 caracter",
+    "Validar nombre no validó que el nombre no sea vacío"
   );
 
   console.assert(
-      validarNombre(
-          '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
-      'Este campo debe tener menos de 50 caracteres',
-      'Validar nombre no validó que el nombre sea menor a 50 caracteres',
+    validarNombre("acbdefghijacbdefghijacbdefghijacbdefghijacbdefghij") ===
+      "Este campo debe tener menos de 50 caracteres",
+    "Validar nombre no validó que el nombre sea menor a 50 caracteres"
   );
 }
 
 probarValidarNombre();
+
+function probarValidarCiudad() {
+  console.assert(
+    validarCiudad("") === "Debes seleccionar una ciudad",
+    "Validar ciudad no validó que hayas seleccionado una ciudad"
+  );
+}
+
+probarValidarCiudad();
+
+function probarValidarDescripcionRegalo() {
+  console.assert(
+    validarDescripcionRegalo("") ===
+      "Este campo debe tener al menos 1 caracter",
+    "Validar descripción del regalo no validó que hayas escrito más de 1 caracter"
+  );
+
+  console.assert(
+    validarDescripcionRegalo(
+      "acbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghijacbdefghija"
+    ) === "Este campo debe tener menos de 500 caracteres",
+    "Validar descripción del regalo no validó que hayas escrito menos de 500 caracteres"
+  );
+}
+
+probarValidarDescripcionRegalo();
